2eabf0d599691bad34f64ff16fdcc6b966e96d51
Wagner Ramos <wagner19ramos@hotmail.com>
2023-07-27 12:38:37 -0300

    Adicionando o pulo do mario, e parando a animção do tubo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-const mario = document.querySelector('mario');
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector(' .pipe');
 
 const jump = () => {
     mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
 }
+
+const loop = setInterval (() => {
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    if (pipePosition<= 100 && pipePosition > 0 && marioPosition < 80){
+
+        pipe.style.animation = '';
+        pipe.style.left = `${pipePosition}px`;
+        
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
